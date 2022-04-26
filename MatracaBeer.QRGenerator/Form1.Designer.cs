470f1a08b40faa4cc8208920cef75cef3074317c
Jorge Eduardo Tamayo Ramos <JorgeT@digitalware.com.co>
2022-04-26 09:30:58 -0500

    Add project files.

---
+++ MatracaBeer.QRGenerator/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace MatracaBeer.QRGenerator
+{
+    partial class frmGenerateQR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.QRImageBox = new System.Windows.Forms.PictureBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnClean = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.QRImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(15, 411);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 28);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generar &QR";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(127, 411);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 28);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // QRImageBox
+            // 
+            this.QRImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.QRImageBox.Location = new System.Drawing.Point(12, 12);
+            this.QRImageBox.Name = "QRImageBox";
+            this.QRImageBox.Size = new System.Drawing.Size(271, 253);
+            this.QRImageBox.TabIndex = 2;
+            this.QRImageBox.TabStop = false;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(12, 305);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(271, 100);
+            this.txtInfo.TabIndex = 3;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 289);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(65, 13);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Información:";
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(208, 411);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(75, 28);
+            this.btnClean.TabIndex = 5;
+            this.btnClean.Text = "&Limpiar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // frmGenerateQR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 451);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.QRImageBox);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGenerate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGenerateQR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Matraca Beer";
+            ((System.ComponentModel.ISupportInitialize)(this.QRImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox QRImageBox;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnClean;
+    }
+}
+
---
+++ MatracaBeer.QRGenerator/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MatracaBeer.QRGenerator
+{
+    public partial class frmGenerateQR : Form
+    {
+        public frmGenerateQR()
+        {
+            InitializeComponent();
+            LoadAndClean();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool generate = true;
+                if (string.IsNullOrWhiteSpace(txtInfo.Text))
+                {
+                    MessageBox.Show(this, "No se encontró información para generar el QR", "Error al generar", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    generate = false;
+                }
+
+                if (generate)
+                {
+                    QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+                    QrCode qrCode = new QrCode();
+                    qrEncoder.TryEncode(txtInfo.Text.Trim(), out qrCode);
+
+                    GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+                        var tmpImage = new Bitmap(ms);
+                        var image = new Bitmap(tmpImage, new Size(new Point(200, 200)));
+                        QRImageBox.BackgroundImage = image;
+                        btnSave.Enabled = true;
+                        btnClean.Enabled = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(this, $"Se presentó un error al generar el código {ex.InnerException}", "Error al generar", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Image image = (Image)QRImageBox.BackgroundImage.Clone())
+                {
+                    saveFileDialog1.AddExtension = true;
+                    saveFileDialog1.Filter = "Image PNG (*.png)|*.png";
+                    saveFileDialog1.ShowDialog();
+                    if (!string.IsNullOrWhiteSpace(saveFileDialog1.FileName))
+                        image.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    else
+                    {
+                        MessageBox.Show("Debe seleccionar una ruta válidad para guardar la imagen");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this,$"Se presentó un error al guardar la imagen {ex.InnerException}", "Error al guardar",MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Metodo para cargar y limpiar los controles al iniciar o al grabar
+        /// </summary>
+        void LoadAndClean()
+        {
+            btnSave.Enabled = false;
+            btnClean.Enabled = false;
+            txtInfo.Text = string.Empty;
+            if (QRImageBox.BackgroundImage != null)
+                QRImageBox.BackgroundImage = null;
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            LoadAndClean();
+        }
+    }
+}
